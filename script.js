3fe381f285d6e631478ca11353f45a41b5a79fef
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-06 23:12:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex=shortHex.replace(/^#/,"");
+	// shortHex=shortHex.toLowerCase();	
+	let fullHex=shortHex
+	 .split('')
+    .map(e=>e.repeat(2))
+    .join('');
+	return `#${fullHex}`;
 };
 
 // Do not change the code below.
