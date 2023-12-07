ee01622dc9b49b9e267bb948f24ec86a968351cc
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-07 11:19:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const extendHex = (shortHex) => {
   // write your code here
-	shortHex=shortHex.replace(/^#/,"");
-	// shortHex=shortHex.toLowerCase();	
-	let fullHex=shortHex
-	 .split('')
-    .map(e=>e.repeat(2))
-    .join('');
-	return `#${fullHex}`;
+	// shortHex=shortHex.replace(/^#/,"");
+	// // shortHex=shortHex.toLowerCase();	
+	// let fullHex=shortHex
+	//  .split('')
+ //    .map(e=>e.repeat(2))
+ //    .join('');
+	// return `#${fullHex}`;
+	shortHex=shortHex.replace("#","");
+	if(shortHex.charAt(0)>="A" && shortHex.charAt(0)<="Z"){
+		shortHex=shortHex.toUpperCase();
+	}
+	let fulHex=shortHex.split("");
+	let fulhex1=fulHex.map(e=>e.repeat(2));
+	fulhex1=fulhex1.join('');
+	fulhex1="#"+fulhex1;
+	return fulhex1;
 };
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+
+
+
